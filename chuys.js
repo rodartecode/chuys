9e72c8e0ec276092f977fdbfb72dde420043ceeb
Jesse <jrodarte@mail.ccsf.edu>
2020-01-08 13:17:21 -0800

    hamburger menu

--- chuys.js
+++ chuys.js
@@ -15,4 +15,19 @@ $(document).ready(function() {
       );
     }
   });
+
+  $(function() {
+    createSticky(jQuery("#header"));
+  });
+
+  function createSticky(sticky) {
+    if (typeof sticky !== "undefined") {
+      var pos = sticky.offset().top,
+        win = jQuery(window);
+
+      win.on("scroll", function() {
+        win.scrollTop() >= pos ? sticky.addClass("fixed") : sticky.removeClass("fixed");
+      });
+    }
+  }
 });
